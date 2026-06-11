d6a9f96028ab0fbf0f7d3a413e6fc23dbc8cf38e
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2026-06-11 14:20:23 -0300

    Update MRLV_PROMOCAOESPECIAL.sql

--- MRLV_PROMOCAOESPECIAL.sql
+++ MRLV_PROMOCAOESPECIAL.sql
@@ -1,36 +1,16 @@
-CREATE OR REPLACE VIEW CONSINCO.MRLV_PROMOCAOESPECIAL AS
-
--- SELECT ORIGINAL:
-/*
-SELECT A.SEQPRODUTO,
-       A.QTDEMBALAGEM,
-       A.NROEMPRESA,
-       A.CODACESSOESPECIAL,
-       A.VLRPRECOPROMOC,
-       A.QTDESOLICITADA,
-       A.DTAINICIO,
-       A.DTAFIM,
-       NVL(A.INDEMIETIQUETA,'N') AS INDEMIETIQUETA,
-       A.SEQPROMOCESPECIAL,
-       A.MOTIVOACAOPROMOC
-FROM MRL_PROMOCESPECIALHIST A
-WHERE A.STATUS = 'A';*/
-
--- NOVO:
-
 CREATE OR REPLACE VIEW MRLV_PROMOCAOESPECIAL AS
 SELECT A.SEQPRODUTO,
        A.QTDEMBALAGEM,
        A.NROEMPRESA,
        A.CODACESSOESPECIAL,
        A.VLRPRECOPROMOC,
-       -- Alt. Giuliano - 26/08/24
-       -- Divide sempre por 2 pois a etiqueta ? dupla
-       -- CEIL arredonda pra cima pois se for solicitado 11, ir? impimir 6 etiquetas (resultando em 12 duplas)
+       -- Alt. Giuliano - 26/08/24 - Controle de quantidade
+       -- Divide sempre por 2 pois a etiqueta e dupla
+       -- CEIL arredonda pra cima pois se for solicitado 11, ira impimir 6 etiquetas (resultando em 12 duplas)
        -- Traz apenas a quantidade nao emitida
        CASE WHEN NVL(A.QTDEETIQEMITIDA,0) = 0 THEN CEIL(A.QTDESOLICITADA/2) ELSE
          CEIL((QTDESOLICITADA - NVL(A.QTDEETIQEMITIDA,0))/2) END QTDESOLICITADA,
-       A.DTAINICIO,
+       NVL(TRUNC(A.DTAHORAPROVACAO), A.DTAINICIO) DTAINICIO, --A.DTAINICIO, -- Ajuste 11/06 para que no grid retorne na data de aprovação, e nao inicio
        A.DTAFIM,
        NVL(A.INDEMIETIQUETA,'N') AS INDEMIETIQUETA,
        A.SEQPROMOCESPECIAL,
@@ -38,8 +18,11 @@ SELECT A.SEQPRODUTO,
 FROM MRL_PROMOCESPECIALHIST A
 WHERE A.STATUS = 'A'
 
- -- Alterado por Giuliano -- Controle de emiss?o
+ -- Alterado por Giuliano -- Controle de emissao
  -- Retornar apenas se a quantidade impressa for menor que a solicitada
 
-  AND NVL(A.QTDEETIQEMITIDA,0) * 2 <= A.QTDESOLICITADA
+  AND 1 - (NVL(A.QTDEETIQEMITIDA,0) * 2) <= A.QTDESOLICITADA
+  AND TRUNC(SYSDATE) BETWEEN A.DTAINICIO AND A.DTAFIM
+  AND A.CODACESSOESPECIAL NOT LIKE '77%'
+  ORDER BY VLRPRECOPROMOC ASC
 ;
